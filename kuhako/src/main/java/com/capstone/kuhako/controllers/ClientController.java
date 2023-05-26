2cbbd537333d6114575cb57b51cbfc98d8d47e3f
kbdiamondes <kbdiamantes@gmail.com>
2023-05-26 13:41:27 +0800

    CorsConfig & Headers
    Added cors config to allow HTTPS connection.

--- kuhako/src/main/java/com/capstone/kuhako/controllers/ClientController.java
+++ kuhako/src/main/java/com/capstone/kuhako/controllers/ClientController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins="*", allowedHeaders = "*")
 public class ClientController {
 
     @Autowired
