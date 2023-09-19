7a4ecc8e7bf14b6149d49f23d2b8c82d52996768
Marc Suarez <marcsuarez31@gmail.com>
2023-09-19 11:44:38 +0800

    change ChatCollector to ChatCollectorController

---
+++ kuhako/src/main/java/com/capstone/kuhako/controllers/ClientModuleController/ChatCollectorController.java
@@ -0,0 +1,37 @@
+package com.capstone.kuhako.controllers.ClientModuleController;
+
+
+import com.capstone.kuhako.models.ClientModules.ChatCollector;
+import com.capstone.kuhako.services.ClientModuleServices.ChatCollectorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class ChatCollectorController {
+    @Autowired
+    ChatCollectorService chatCollectorService;
+
+    @RequestMapping(value="/chatCollector", method = RequestMethod.POST)
+    public ResponseEntity<Object> createChatCollector(@RequestBody ChatCollector chatCollector) {
+        chatCollectorService.createChatCollector(chatCollector);
+        return new ResponseEntity<>("ChatCollector created successfully", HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value="/chatCollector", method = RequestMethod.GET)
+    public ResponseEntity<Object> getChatCollector() {
+        return new ResponseEntity<>(chatCollectorService.getChatCollector(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/chatCollector/{chatCollectorid}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteChatCollector(@PathVariable Long chatCollectorid) {
+        return chatCollectorService.deleteChatCollector(chatCollectorid);
+    }
+
+    @RequestMapping(value="/chatCollector/{chatCollectorid}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateChatCollector(@PathVariable Long chatCollectorid, @RequestBody ChatCollector chatCollector) {
+        return chatCollectorService.updateChatCollector(chatCollectorid, chatCollector);
+    }
+}
