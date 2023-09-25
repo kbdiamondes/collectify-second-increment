0f4636aa80e2a525c5103d7f58dc5e269cf5aedb
Marc Suarez <marcsuarez31@gmail.com>
2023-09-25 23:58:38 +0800

    fixed 3rd collector with working authentication

--- kuhako/src/main/java/com/capstone/kuhako/controllers/CollectorModuleController/CollectPaymentsController.java
+++ kuhako/src/main/java/com/capstone/kuhako/controllers/CollectorModuleController/CollectPaymentsController.java
@@ -1,7 +1,9 @@
 package com.capstone.kuhako.controllers.CollectorModuleController;
 
 
+import com.capstone.kuhako.models.Collector;
 import com.capstone.kuhako.models.CollectorModules.CollectPayments;
+import com.capstone.kuhako.repositories.CollectorRepository;
 import com.capstone.kuhako.services.CollectorModuleServices.CollectPaymentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,11 +18,18 @@ import org.springframework.web.bind.annotation.*;
 public class CollectPaymentsController {
     @Autowired
     CollectPaymentsService collectPaymentsService;
+    @Autowired
+    private CollectorRepository collectorRepository;
 
-    @RequestMapping(value="/collectPayments", method = RequestMethod.POST)
-    public ResponseEntity<Object> createCollectPayments(@RequestBody CollectPayments collectPayments) {
-        collectPaymentsService.createCollectPayments(collectPayments);
-        return new ResponseEntity<>("CollectPayments created successfully", HttpStatus.CREATED);
+    @RequestMapping(value="/collectPayments/{collectorId}", method = RequestMethod.POST)
+    public ResponseEntity<Object> createCollectPayments(@PathVariable Long collectorId, @RequestBody CollectPayments collectPayments) {
+        Collector collector = collectorRepository.findById(collectorId).orElse(null);
+        if (collector != null) {
+            collectPaymentsService.createCollectPayments(collectorId, collectPayments);
+            return new ResponseEntity<>("CollectPayments created successfully", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("Collector does not exist", HttpStatus.NOT_FOUND);
+        }
     }
 
     @RequestMapping(value="/collectPayments", method = RequestMethod.GET)
@@ -28,13 +37,18 @@ public class CollectPaymentsController {
         return new ResponseEntity<>(collectPaymentsService.getCollectPayments(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/collectPayments/{collectPaymentsid}", method = RequestMethod.DELETE)
-    public ResponseEntity<Object> deleteCollectPayments(@PathVariable Long collectPaymentsid) {
-        return collectPaymentsService.deleteCollectPayments(collectPaymentsid);
+    @RequestMapping(value="/collectPayments/collector/{collectorId}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getCollectPaymentsByCollectorId(@PathVariable Long collectorId) {
+        return new ResponseEntity<>(collectPaymentsService.getCollectPaymentsByCollectorId(collectorId), HttpStatus.OK);
     }
 
-    @RequestMapping(value="/collectPayments/{collectPaymentsid}", method = RequestMethod.PUT)
-    public ResponseEntity<Object> updateCollectPayments(@PathVariable Long collectPaymentsid, @RequestBody CollectPayments collectPayments) {
-        return collectPaymentsService.updateCollectPayments(collectPaymentsid, collectPayments);
+    @RequestMapping(value = "/collectPayments/{collectorId}/{collectPaymentsId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteCollectPayments(@PathVariable Long collectorId, @PathVariable Long collectPaymentsId) {
+        return collectPaymentsService.deleteCollectPayments(collectorId, collectPaymentsId);
+    }
+
+    @RequestMapping(value="/collectPayments/{collectorId}/{collectPaymentsId}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateCollectPayments(@PathVariable Long collectorId, @PathVariable Long collectPaymentsId, @RequestBody CollectPayments collectPayments) {
+        return collectPaymentsService.updateCollectPayments(collectorId, collectPaymentsId, collectPayments);
     }
 }
