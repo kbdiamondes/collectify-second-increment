e9a39da79781f14204531bec6cbed6e70f553238
francisjas <francisjasper.manto@cit.edu>
2023-10-03 19:10:16 +0800

    my collectors service is added

--- kuhako/src/main/java/com/capstone/kuhako/models/Reseller.java
+++ kuhako/src/main/java/com/capstone/kuhako/models/Reseller.java
@@ -8,11 +8,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.util.Set;
 
+
+
+
 @Entity
 @Table(name = "reseller")
 public class Reseller {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long reseller_id;
 
     @OneToMany(mappedBy = "reseller", cascade = CascadeType.ALL)
